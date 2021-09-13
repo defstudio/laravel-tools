ee9c58d4e1ed88e22ec1ea97a70896d15056bb17
Fabio Ivona <fabio.ivona@defstudio.it>
2021-09-13 18:40:42 +0200

    fix

--- src/View/Components/Card.php
+++ src/View/Components/Card.php
@@ -5,15 +5,10 @@ namespace DefStudio\Tools\View\Components;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
-use Livewire\Component;
+use Illuminate\View\Component;
 
 class Card extends Component
 {
-    public function __construct($id = null)
-    {
-        parent::__construct($id);
-    }
-
     public function render(): Factory|View|Application
     {
         return view('tools::card');
