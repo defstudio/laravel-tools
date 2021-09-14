65b7c7fefe62dc1f1af38976b9064fcdf4240aae
Fabio Ivona <fabio.ivona@defstudio.it>
2021-09-14 14:27:23 +0200

    adds actions and header to card component

--- resources/views/card.blade.php
+++ resources/views/card.blade.php
@@ -1,3 +1,26 @@
-<div {{$attributes->merge(['class' => 'bg-white overflow-hidden shadow-xl sm:rounded-lg'])}}>
-    {{$slot}}
+<div>
+    @if(isset($header))
+        <div class="flex items-center justify-start px-4 py-3 bg-gray-50 sm:px-6 shadow sm:rounded-tl-md sm:rounded-tr-md">
+            {{ $header }}
+        </div>
+    @endif
+
+    <div @class([
+        'py-4',
+        'py-5',
+        'bg-white',
+        'sm:p-6',
+        'shadow',
+        'sm:rounded-md',
+        'sm:rounded-tl-md sm:rounded-tr-md' => isset($actions)
+    ])>
+        {{$slot}}
+    </div>
+
+    @if(isset($actions))
+        <div class="flex items-center justify-end px-4 py-3 bg-gray-50 text-right sm:px-6 shadow sm:rounded-bl-md sm:rounded-br-md">
+            {{ $actions }}
+        </div>
+    @endif
 </div>
+
