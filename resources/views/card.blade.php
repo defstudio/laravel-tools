9620483c2cc2f953d735a183d657d6111f5123ba
Fabio Ivona <fabio.ivona@defstudio.it>
2021-09-15 18:25:33 +0200

    adds transitions

--- resources/views/card.blade.php
+++ resources/views/card.blade.php
@@ -1,22 +1,33 @@
 <div x-data="{
             show:{{$collapsed?'false':'true'}},
-            collapsable:{{$collapsable?'true':'false'}}
     }">
     @if(isset($header))
-        <div @click='show=!show'
+        <div
             @class([
                'flex items-center justify-start',
                'px-4 py-3 sm:px-6',
                'bg-gray-50',
                'shadow sm:rounded-tl-md sm:rounded-tr-md',
-               'cursor-pointer' => $collapsable
             ])
         >
             {{ $header }}
+
+            @if($collapsable)
+                <svg @click='show=!show' x-bind:class="show ? 'rotate-180' : ''" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 ml-auto cursor-pointer transition-transform duration-700" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 13l-3 3m0 0l-3-3m3 3V8m0 13a9 9 0 110-18 9 9 0 010 18z" />
+                </svg>
+            @endif
         </div>
     @endif
 
-    <div x-show="show" @class([
+    <div x-show="show"
+         x-transition:enter="transition-transform transition-opacity ease-out duration-300"
+         x-transition:enter-start="opacity-0 transform -translate-y-2"
+         x-transition:enter-end="opacity-100 transform translate-y-0"
+         x-transition:leave="transition ease-in duration-300"
+         x-transition:leave-start="opacity-100 transform translate-y-0"
+         x-transition:leave-end="opacity-0 transform -translate-y-3"
+        @class([
         'py-4 py-5 sm:p-6',
         'bg-white',
         'shadow',
@@ -27,7 +38,7 @@
     </div>
 
     @if(isset($actions))
-        <div  x-show="!collapsable || show" class="flex items-center justify-end px-4 py-3 bg-gray-50 text-right sm:px-6 shadow sm:rounded-bl-md sm:rounded-br-md">
+        <div  x-show="show" class="flex items-center justify-end px-4 py-3 bg-gray-50 text-right sm:px-6 shadow sm:rounded-bl-md sm:rounded-br-md">
             {{ $actions }}
         </div>
     @endif
