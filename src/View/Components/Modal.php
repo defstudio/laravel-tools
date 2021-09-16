5cc1ba26d56f9f4fe0880ea2dae5ba1d5bffdf79
Fabio Ivona <fabio.ivona@defstudio.it>
2021-09-16 19:41:06 +0200

    minor fixes

--- src/View/Components/Modal.php
+++ src/View/Components/Modal.php
@@ -5,8 +5,9 @@ namespace DefStudio\Tools\View\Components;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
 
-class Modal extends \Illuminate\View\Component
+class Modal extends Component
 {
     public function __construct(
         public string|null $id = null,
