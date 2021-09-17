7e9e3c6339a4a72e32d99aa8b49a7ab21b3f9bc3
Fabio Ivona <fabio.ivona@defstudio.it>
2021-09-17 16:09:36 +0200

    fix

--- resources/views/button.blade.php
+++ resources/views/button.blade.php
@@ -6,9 +6,9 @@ $color_classes = match ($color) {
     'primary' =>   'bg-gray-800   border-transparent       text-white      hover:bg-gray-700 active:bg-gray-900  focus:border-gray-900  focus:ring-gray-300 ',
     'secondary' => 'bg-white      border-gray-300          text-gray-700   shadow-sm hover:text-gray-500         focus:border-blue-300  focus:ring-blue-200    active:text-gray-800 active:bg-gray-50 ',
     'danger' =>    'bg-red-600    border-transparent       text-white      hover:bg-red-500                      focus:border-red-700   focus:ring-red-200     active:bg-red-600',
-}
+};
 
-$class = "inline-flex items-center px-4 py-2 border rounded-md font-semibold text-xs uppercase tracking-widest focus:outline-none focus:ring disabled:opacity-25 transition $color_classes"
+$class = "inline-flex items-center px-4 py-2 border rounded-md font-semibold text-xs uppercase tracking-widest focus:outline-none focus:ring disabled:opacity-25 transition $color_classes";
 ?>
 
 @if($type == 'link')
