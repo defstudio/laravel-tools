323ba1b5e39af70016174204b6677b10e7564c74
Fabio Ivona <fabio.ivona@defstudio.it>
2021-09-17 18:24:45 +0200

    fix

--- resources/views/button.blade.php
+++ resources/views/button.blade.php
@@ -4,13 +4,13 @@
 
 
 $color_classes = match ($color) {
-    'primary' =>   'bg-gray-800   border-transparent       text-white      hover:bg-gray-700 active:bg-gray-900  focus:border-gray-900  focus:ring-gray-300 ',
+    'primary' => 'bg-gray-800   border-transparent       text-white      hover:bg-gray-700 active:bg-gray-900  focus:border-gray-900  focus:ring-gray-300 ',
     'secondary' => 'bg-white      border-gray-300          text-gray-700   shadow-sm hover:text-gray-500         focus:border-blue-300  focus:ring-blue-200    active:text-gray-800 active:bg-gray-50 ',
-    'danger' =>    'bg-red-600    border-transparent       text-white      hover:bg-red-500                      focus:border-red-700   focus:ring-red-200     active:bg-red-600',
+    'danger' => 'bg-red-600    border-transparent       text-white      hover:bg-red-500                      focus:border-red-700   focus:ring-red-200     active:bg-red-600',
 };
 
-$size_classes = match ($size){
-    'sm' => 'px-1 py-1',
+$size_classes = match ($size) {
+    'sm' => 'px-2 py-1',
     default => 'px-4 py-2'
 };
 
