0bd53315579c1a5e082fad3d9313662eb21cab00
nicola-posa <80521158+nicola-posa@users.noreply.github.com>
2022-02-02 10:20:20 +0100

    Update ConfirmsActions.php
    Implementazione conferma con password utente

--- src/Traits/ConfirmsActions.php
+++ src/Traits/ConfirmsActions.php
@@ -2,6 +2,8 @@
 
 namespace DefStudio\Tools\Traits;
 
+use Illuminate\Contracts\Auth\StatefulGuard;
+
 /**
  * @mixin \Livewire\Component
  */
@@ -9,27 +11,50 @@ trait ConfirmsActions
 {
     public bool $confirming_action = false;
     public string|null $confirmable_id = null;
+    public bool $requires_password = false;
+    public string $password_confirmation = '';
 
-    public function start_confirming_action(string $confirmable_id): void
+    public function start_confirming_action(string $confirmable_id, bool $requires_password): void
     {
         $this->confirming_action = true;
         $this->confirmable_id = $confirmable_id;
-
-        $this->dispatchBrowserEvent('confirming-action');
+        $this->requires_password = $requires_password;
     }
 
     public function stop_confirming_action(): void
     {
         $this->confirming_action = false;
         $this->confirmable_id = null;
+        $this->requires_password = false;
+        $this->password_confirmation = '';
     }
 
     public function confirmation_given(): void
     {
+        if(!$this->confirm_password()){
+            return;
+        }
+        
         $this->dispatchBrowserEvent('action-confirmed', [
             'id' => $this->confirmable_id,
         ]);
 
         $this->stop_confirming_action();
+    }
+    
+    public function confirm_password(): bool
+    {
+        if(!this->requires_password){
+            return true;
+        }
+        
+        $user = user();
+        
+        $guard = app(StatefulGuard::class);
+        
+        return $guard->validate([
+            'email' => $user->email,
+            'password' => $password,
+        ]);
     }
 }
