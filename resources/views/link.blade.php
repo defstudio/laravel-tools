b6f260a38bef45ba61b6fa2659b0fc01ded9e650
Fabio Ivona <fabio.ivona@defstudio.it>
2022-02-08 16:53:54 +0100

    fix link

--- resources/views/link.blade.php
+++ resources/views/link.blade.php
@@ -1,11 +1,6 @@
-<?php
-
-if ($attributes->wire('loading.spinner')->value === true) {
-    $wire_loading_spin = $attributes->wire('click')->value;
-}
-
-?>
-
 <a {{ $attributes->merge(['class' => $base_class()]) }}>
-    <x-button-content :wire-loading-spin="$wire_loading_spin??''" :icon="$icon">{{$slot}}</x-button-content>
+    @if(!empty($icon))
+        <x-icon wire:loading.remove wire:target="{!! $wireLoadingSpin !!}" :name="$icon"/>
+    @endif
+    {{ $slot }}
 </a>
