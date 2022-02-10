60b800ba9811ad201d6537902b12dfe3f9bc7044
Fabio Ivona <fabio.ivona@defstudio.it>
2022-02-10 18:52:19 +0100

    fix

--- resources/views/link.blade.php
+++ resources/views/link.blade.php
@@ -1,6 +1,6 @@
 <a {{ $attributes->merge(['class' => $base_class()]) }}>
     @if(!empty($icon))
-        <x-icon wire:loading.remove wire:target="{!! $wireLoadingSpin !!}" :name="$icon"/>
+        <x-icon wire:loading.remove :name="$icon"/>
     @endif
     {{ $slot }}
 </a>
