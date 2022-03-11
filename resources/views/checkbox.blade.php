ba2370d0c04a3142f0c1817f785f082afbe8a816
Fabio Ivona <fabio.ivona@defstudio.it>
2022-03-11 22:09:38 +0100

    Create checkbox.blade.php

---
+++ resources/views/checkbox.blade.php
@@ -0,0 +1,21 @@
+<div {{$attributes}}>
+    <input id="{{$id}}"
+           type="checkbox"
+           class='rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50'
+           autocomplete="none"
+           @if($model)wire:model{{$defer ? '.defer' : ''}}="{{$model}}"@endif
+    />
+
+    @if(!empty($label))
+        <label for="{{$id}}" class='font-medium text-sm text-gray-700'>
+            {{$label}}
+        </label>
+    @endif
+
+    @if($model)
+        @error($model)
+        <p class='text-sm text-red-600 mt-2'>{{ $message }}</p>
+        @enderror
+    @endif
+
+</div>
