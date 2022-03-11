d21028478fcd063c1969833dacc9fe023a8c1275
Fabio Ivona <fabio.ivona@defstudio.it>
2022-03-11 22:10:09 +0100

    Create Checkbox.php

---
+++ src/View/Components/Checkbox.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace DefStudio\Tools\View\Components;
+
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Support\Str;
+use Illuminate\View\Component;
+
+class Checkbox extends WiredInputComponent
+{
+}
