5ff752664ff3aef929493387392346e7682f1892
Fabio Ivona <fabio.ivona@defstudio.it>
2022-03-12 23:41:54 +0100

    feat: SmartFinds

---
+++ src/Concerns/SmartFinds.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace DefStudio\Tools\Concerns;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @mixin Model
+ */
+trait SmartFinds
+{
+    public static function of(self|int $id): self
+    {
+        return $id instanceof self
+            ? $id
+            : self::findOrFail($id);
+    }
+}
