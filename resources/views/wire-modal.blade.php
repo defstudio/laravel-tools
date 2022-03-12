7facd373bf68a35d6100c84a41c28d92e7eb5838
Fabio Ivona <fabio.ivona@defstudio.it>
2022-03-12 09:14:28 +0100

    Update wire-modal.blade.php

--- resources/views/wire-modal.blade.php
+++ resources/views/wire-modal.blade.php
@@ -1,7 +1,9 @@
 <div class="p-2">
-    <div wire:click="$emit('closeModal')" class="absolute top-0 right-0 w-6 h-6 rounded-bl-lg border shadow bg-gray-100 flex justify-center items-center cursor-pointer">
-        <x-icon size="4" name="x"/>
-    </div>
+    @if($xClose)
+        <div wire:click="$emit('closeModal')" class="absolute top-0 right-0 w-6 h-6 rounded-bl-lg border shadow bg-gray-100 flex justify-center items-center cursor-pointer">
+            <x-icon size="4" name="x"/>
+        </div>
+    @endif
 
     @unless(empty($title))
         <div class="text-lg underline mb-4">{{$title}}</div>
