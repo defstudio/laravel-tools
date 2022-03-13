8a222d2e9c7e8da60b37880710e181f2a3713004
Fabio Ivona <fabio.ivona@defstudio.it>
2022-03-13 11:42:22 +0100

    fix button icon margin

--- resources/views/button.blade.php
+++ resources/views/button.blade.php
@@ -24,7 +24,7 @@ if (!empty($wire_loading_disabled)) {
 <button type="{{$type}}"
     {{ $attributes->merge(['class' => $base_class()])}}>
     @if(!empty($icon))
-        <x-icon :name="$icon"/>
+        <x-icon class="mr-2" :name="$icon"/>
     @endif
     {{ $slot }}
 </button>
