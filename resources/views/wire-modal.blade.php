ca7276847626f373298b62bfdab6286108bc7022
Fabio Ivona <fabio.ivona@defstudio.it>
2022-04-04 16:48:52 +0200

    fix

--- resources/views/wire-modal.blade.php
+++ resources/views/wire-modal.blade.php
@@ -1,18 +1,22 @@
+<?php
+/** @var \Illuminate\View\ComponentAttributeBag $attributes */
+?>
+
 <div class="p-2">
     @if($xClose)
         <div wire:click="$emit('closeModal')" class="absolute top-0 right-0 w-6 h-6 rounded-bl-lg border shadow bg-gray-100 flex justify-center items-center cursor-pointer">
             <x-icon size="4" name="x"/>
         </div>
     @endif
-
+    
     @unless(empty($title))
         <div class="text-lg underline mb-4">{{$title}}</div>
     @endunless
-
-    <div class="{{$grid ? "md:grid md:grid-cols-$grid md:gap-6" : ''}}">
+    
+    <div {{$attributes->class($grid ? "md:grid md:grid-cols-$grid md:gap-6" : '')}}>
         {{$slot}}
     </div>
-
+    
     @if(!empty($actions))
         <div class="mt-6 flex flex-row-reverse justify-between">
             @if(is_array($actions))
