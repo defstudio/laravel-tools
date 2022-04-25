fc1788263f0dcd060f2fcfbfc69f8fc104dd8fa4
Fabio Ivona <fabio.ivona@defstudio.it>
2022-04-25 13:44:14 +0200

    Update text.blade.php

--- resources/views/text.blade.php
+++ resources/views/text.blade.php
@@ -2,6 +2,9 @@
     @if(!empty($label))
         <label for="{{$id}}" class='block font-medium text-sm text-gray-700'>
             {{$label}}
+            @if($attributes->has('required') && $attributes->get('required'))
+                <span class="text-red-500">&nbsp;*</span>
+            @endif
         </label>
     @endif
 
