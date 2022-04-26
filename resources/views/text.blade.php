33775838eaaad1994c178c6d33e4c72b48bf89cc
Fabio Ivona <fabio.ivona@defstudio.it>
2022-04-26 12:06:56 +0200

    Update text.blade.php

--- resources/views/text.blade.php
+++ resources/views/text.blade.php
@@ -8,12 +8,27 @@
         </label>
     @endif
 
-    <input id="{{$id}}"
-           type="text"
-           class='{{$base_class()}}'
-           autocomplete="off"
-           @if($model)wire:model{{$defer ? '.defer' : ''}}="{{$model}}"@endif
-    />
+    <div @class([
+        "relative rounded-md shadow-sm",
+        $wFull => 'w-full',
+    ])>
+        @isset($prefix)
+            <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                <span class="text-gray-500 sm:text-sm"> {{$prefix}} </span>
+            </div>
+        @endisset
+        <input id="{{$id}}"
+               type="text"
+               autocomplete="off"
+               class='{{$base_class()}} {{isset($prefix) ? 'pl-7': ''}} {{isset($postfix) ? 'pr-7': ''}}'
+               @if($model)wire:model{{$defer ? '.defer' : ''}}="{{$model}}"@endif
+        />
+        @isset($postfix)
+            <div class="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
+                <span class="text-gray-500 sm:text-sm"> {{$postfix}} </span>
+            </div>
+        @endisset
+    </div>
 
     @if($model)
         @error($model)
