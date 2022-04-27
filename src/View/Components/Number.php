75063a17b67b65601bfa85d95e9c79fba78159ea
Fabio Ivona <fabio.ivona@defstudio.it>
2022-04-27 17:27:00 +0200

    Update Number.php

--- src/View/Components/Number.php
+++ src/View/Components/Number.php
@@ -10,5 +10,20 @@ use Illuminate\View\Component;
 
 class Number extends WiredInputComponent
 {
+    public function __construct(
+        ?string $id = null,
+        ?string $label = null,
+        bool $defer = false,
+        ?string $model = null,
+        string $size = 'normal',
+        bool $wFull = true,
+        string $color = 'indigo',
+        public int|float|null $min = null,
+        public int|float|null $max = null,
+        public int|float|null $step = null,
+    )
+    {
+        parent::__construct($id, $label, $defer, $model, $size, $wFull, $color);
+    }
 
 }
