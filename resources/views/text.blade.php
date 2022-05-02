9dfebbc72d58be2f4a9372a5c3406cd14614c7d5
Fabio Ivona <fabio.ivona@defstudio.it>
2022-05-02 16:29:04 +0200

    Update text.blade.php

--- resources/views/text.blade.php
+++ resources/views/text.blade.php
@@ -1,5 +1,13 @@
-<div {{$attributes}}>
+<?php
 
+use Illuminate\View\ComponentSlot;
+
+/** @var ComponentSlot $prefix */
+/** @var ComponentSlot $postfix */
+?>
+
+
+<div {{$attributes}}>
     @if(!empty($label))
         <label for="{{$id}}" class='block font-medium text-sm text-gray-700'>
             {{$label}}
@@ -9,13 +17,34 @@
         </label>
     @endif
 
-    <textarea id="{{$id}}" class="{{$base_class()}}"
-              @if($model)wire:model{{$defer ? '.defer' : ''}}="{{$model}}"@endif
-    >{!! $slot !!}</textarea>
+    <div @class([
+        "flex rounded-md shadow-sm",
+        $wFull => 'w-full',
+        "border border-gray-300 focus:border-$color-300",
+        "focus:ring focus:ring-$color-200 focus:ring-opacity-50",
+    ])>
+        @if(isset($prefix) && $prefix->isNotEmpty())
+            <div class="pl-2 flex items-center pointer-events-none">
+                <span class="text-gray-500 sm:text-sm"> {{$prefix}} </span>
+            </div>
+        @endif
+        <input id="{{$id}}"
+               type="text"
+               autocomplete="off"
+               class='{{$base_class(false)}}'
+               @if($model)wire:model{{$defer ? '.defer' : ''}}="{{$model}}"@endif
+        />
+        @if(isset($postfix) && $postfix->isNotEmpty())
+            <div class="pr-2 flex items-center pointer-events-none">
+                <span class="text-gray-500 sm:text-sm"> {{$postfix}} </span>
+            </div>
+        @endif
+    </div>
 
     @if($model)
         @error($model)
         <p class='text-sm text-red-600 mt-2'>{{ $message }}</p>
         @enderror
     @endif
+
 </div>
