5627ee07342451b0978af6d737243818ac9ed346
Fabio Ivona <fabio.ivona@defstudio.it>
2022-05-02 16:23:12 +0200

    Update Checkbox.php

--- src/View/Components/Checkbox.php
+++ src/View/Components/Checkbox.php
@@ -10,4 +10,17 @@ use Illuminate\View\Component;
 
 class Checkbox extends WiredInputComponent
 {
+    public function __construct(
+        ?string $id = null,
+        ?string $label = null,
+        bool $defer = false,
+        ?string $model = null,
+        string $size = 'normal',
+        bool $wFull = true,
+        string $color = 'indigo',
+        public bool $alignWithOthers = false,
+    ) {
+        parent::__construct($id, $label, $defer, $model, $size, $wFull, $color);
+    }
+
 }
