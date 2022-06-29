f1eb9c334d3e960a163c23a2222ed2f4bdf7c3ae
Fabio Ivona <fabio.ivona@defstudio.it>
2022-06-29 17:06:59 +0200

    fix checkbox layout

--- resources/views/checkbox.blade.php
+++ resources/views/checkbox.blade.php
@@ -2,20 +2,23 @@
     @if($alignWithOthers)
         <label>&nbsp;</label>
     @endif
-
-    <div class="grow flex items-center">
-        <input id="{{$id}}"
-               type="checkbox"
-               class='rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 cursor-pointer'
-               @if($model)wire:model{{$defer ? '.defer' : ''}}="{{$model}}"@endif
-        />
-
-        @if(!empty($label))
-            <label for="{{$id}}" class='font-medium text-sm text-gray-700 cursor-pointer'>
-                &nbsp;{{$label}}
-            </label>
-        @endif
-
+    
+    <div class="grow">
+        <div>
+            <input id="{{$id}}"
+                   type="checkbox"
+                   class='rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 cursor-pointer'
+                   @if($model)wire:model{{$defer ? '.defer' : ''}}="{{$model}}"@endif
+            />
+            
+            @if(!empty($label))
+                <label for="{{$id}}" class='font-medium text-sm text-gray-700 cursor-pointer'>
+                    &nbsp;{{$label}}
+                </label>
+            @endif
+        </div>
+        
+        
         @if($model && $showErrors)
             @error($model)
             <p class='text-sm text-red-600 mt-2'>{{ $message }}</p>
