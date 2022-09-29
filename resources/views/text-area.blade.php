52d7fdf0eeda011973a3b297b740a9a27f172e21
Fabio Ivona <fabio.ivona@defstudio.it>
2022-09-29 12:59:40 +0200

    update text-area

--- resources/views/text-area.blade.php
+++ resources/views/text-area.blade.php
@@ -1,3 +1,28 @@
-<textarea {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm']) !!}
-          placeholder="{{$hint}}"
->{!! $slot !!}</textarea>
+<div {{$attributes}}>
+    @if(!empty($label))
+        <label for="{{$id}}" class='block font-medium text-sm text-gray-700'>
+            {{$label}}
+            @if($attributes->has('required') && $attributes->get('required'))
+                <span class="text-red-500">&nbsp;*</span>
+            @endif
+        </label>
+    @endif
+    
+    <div @class([
+        "flex rounded-md shadow-sm",
+        $wFull => 'w-full',
+        "border border-gray-300 focus:border-$color-300",
+        "focus:ring focus:ring-$color-200 focus:ring-opacity-50",
+    ])>
+    <textarea id="{{$id}}"
+              class='{{$base_class(false)}}'
+              placeholder="{{$hint}}"
+              @if($model)wire:model{{$defer ? '.defer' : ''}}="{{$model}}"@endif
+    >{!! $slot !!}</textarea>
+    </div>
+    @if($model && $showErrors)
+        @error($model)
+        <p class='text-sm text-red-600 mt-2'>{{ $message }}</p>
+        @enderror
+    @endif
+</div>
