4a3f2505dd00863e562fc0727b53b5870f8e69a2
Fabio Ivona <fabio.ivona@defstudio.it>
2022-10-07 12:03:36 +0200

    fix

--- resources/views/card.blade.php
+++ resources/views/card.blade.php
@@ -1,17 +1,17 @@
-<div {{$attributes}} x-data="{
+<div {{$attributes->class('grid grid-cols-1 place-items-stretch')}} x-data="{
             show:{{$collapsed?'false':'true'}},
     }">
     @if(isset($header))
         <div
-            @class([
-               'flex items-center justify-start',
-               'px-4 py-3 sm:px-6',
-               'bg-gray-50',
-               'shadow sm:rounded-tl-md sm:rounded-tr-md',
-            ])
+                @class([
+                   'flex items-center justify-start',
+                   'px-4 py-3 sm:px-6',
+                   'bg-gray-50',
+                   'shadow sm:rounded-tl-md sm:rounded-tr-md',
+                ])
         >
             {{ $header }}
-
+            
             @if($collapsable)
                 <svg @click='show=!show' x-bind:class="show ? 'rotate-180' : ''" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 ml-auto cursor-pointer transition-transform duration-700" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 13l-3 3m0 0l-3-3m3 3V8m0 13a9 9 0 110-18 9 9 0 010 18z" />
@@ -19,7 +19,7 @@
             @endif
         </div>
     @endif
-
+    
     <div x-show="show"
          x-transition:enter="transition-transform transition-opacity ease-out duration-300"
          x-transition:enter-start="opacity-0 transform -translate-y-2"
@@ -27,16 +27,16 @@
          x-transition:leave="transition ease-in duration-300"
          x-transition:leave-start="opacity-100 transform translate-y-0"
          x-transition:leave-end="opacity-0 transform -translate-y-3"
-        @class([
-        'p-4',
-        'bg-white',
-        'shadow',
-        'sm:rounded-bl-md sm:rounded-br-md' => !isset($actions),
-        'sm:rounded-tl-md sm:rounded-tr-md' => !isset($header),
-    ]) >
+            @class([
+            'p-4',
+            'bg-white',
+            'shadow',
+            'sm:rounded-bl-md sm:rounded-br-md' => !isset($actions),
+            'sm:rounded-tl-md sm:rounded-tr-md' => !isset($header),
+        ]) >
         {{$slot}}
     </div>
-
+    
     @if(isset($actions))
         <div  x-show="show" class="flex items-center justify-end px-4 py-3 bg-gray-50 text-right sm:px-6 shadow sm:rounded-bl-md sm:rounded-br-md">
             {{ $actions }}
