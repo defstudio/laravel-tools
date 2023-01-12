8528fc2fe938300b7e7945f452e2eee7ef299178
Fabio Ivona <fabio.ivona@defstudio.it>
2023-01-12 10:52:31 +0100

    Update BackupsAttributes.php

--- src/Concerns/Models/BackupsAttributes.php
+++ src/Concerns/Models/BackupsAttributes.php
@@ -9,17 +9,31 @@ use Illuminate\Database\Eloquent\Model;
  */
 trait BackupsAttributes
 {
+    protected static bool $_attributes_backup_enabled = true;
     protected array $_attributes_backup = [];
 
     public static function bootBackupsAttributes(): void
     {
         self::retrieved(function (self $model) {
+            if(!self::$_attributes_backup_enabled){
+                return;
+            }
+
             $model->_attributes_backup = $model->attributesToArray();
         });
 
         self::created(function (self $model) {
+            if(!self::$_attributes_backup_enabled){
+                return;
+            }
+
             $model->_attributes_backup = $model->attributesToArray();
         });
+    }
+
+    public static function disableAttributesBackup(): void
+    {
+        self::$_attributes_backup_enabled = false;
     }
 
     public function get_backup(string $attribute): mixed
