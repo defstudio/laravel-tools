c468c0fe58f1bf9c1c05fb86b59ceed11cf0c3a1
Fabio Ivona <fabio.ivona@defstudio.it>
2023-06-17 18:02:55 +0200

    Update checkbox.blade.php

--- resources/views/checkbox.blade.php
+++ resources/views/checkbox.blade.php
@@ -13,6 +13,7 @@ $attributes = $attributes->except(['disabled', '$required']);
         <div>
             <input id="{{$id}}"
                    type="checkbox"
+                   value="{{$value}}"
                    class='rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 cursor-pointer'
                    {{$disabled ? 'disabled' : ''}}
                    {{$required ? 'required' : ''}}
