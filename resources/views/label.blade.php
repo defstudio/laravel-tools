ae6d4a91d5f1194983067653485807c26504fe28
Fabio Ivona <fabio.ivona@defstudio.it>
2023-08-07 16:06:07 +0200

    label
    Took 41 seconds

--- resources/views/label.blade.php
+++ resources/views/label.blade.php
@@ -1,5 +1,5 @@
 
-<label {{$attributes->class(['block font-medium text-sm text-gray-700'])}}>{{$label}}
+<label {{$attributes->class(['block font-medium text-sm text-gray-700'])}}>{{$slot}}
     @if($attributes->has('required') && $attributes->get('required'))
         <span class="text-red-500">&nbsp;*</span>
     @endif
