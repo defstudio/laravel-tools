9a217d9eb0f66b55c05ad3f37f7bea381892ca71
Fabio Ivona <fabio.ivona@defstudio.it>
2024-04-10 12:25:46 +0200

    Update ConfirmAction.php

--- src/View/Components/ConfirmAction.php
+++ src/View/Components/ConfirmAction.php
@@ -14,7 +14,7 @@ class ConfirmAction extends Component
         public string $content = 'Do you really want it?',
         public string $confirmText = 'Confirm',
         public string $abortText = 'No, Abort',
-        public string $color = 'none',
+        public string $color = 'primary',
         public string|bool $requiresPassword = '',
     ) {
         if ($this->requiresPassword === true) {
